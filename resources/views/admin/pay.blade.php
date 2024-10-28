0bfa18be2a7384137459963dd90b615aadfc4dc3
coderXgourav <softwaredevelopergourav@gmail.com>
2024-10-28 15:47:26 +0530

    payment form add

---
+++ resources/views/admin/pay.blade.php
@@ -0,0 +1,23 @@
+
+<div class="content">
+    <div class="links">
+        <form action="/payment" method="POST">
+            <input type="hidden" name="_token" value="oCRl5zAh2taYC5WvR5Xe456GyyZ48L8UergQTCt9" autocomplete="off">
+            <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                data-key="pk_test_51OJwqlIQugzZQunOA8CjZJJEUPE8YLy7lTjkSIW85UZoplYMlhNKye2y8s4xdmd9QaqmEFuR87kI9JPpN7BLxWdg00UuuHPQyM"
+                data-amount="1000" data-name="Oradah" data-description="Payment Form"
+                data-image="/assets/images/logo-icon.png" data-locale="auto" data-currency="usd"></script>
+        </form>
+    </div>
+</div>
+</div>
+<script src="https://prism.kyptronixllp.co.in/common_js/jquery.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.stripe-button-el').click();
+    });
+</script>
+
+</body>
+
+</html>
