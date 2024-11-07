5d1560a34ef9a4d65d1fb0f38f4af314ae28033d
srimayeekyptronix1234 <srimayee.kyptronix@gmail.com>
2024-11-07 16:04:50 +0530

    remarks

--- resources/views/admin/dashboard/leads_view.blade.php
+++ resources/views/admin/dashboard/leads_view.blade.php
@@ -42,18 +42,17 @@
                          <a href="{{route('admin.send-message',['id'=>$id])}}" class="btn btn-secondary"><i class="fa fa-commenting" aria-hidden="true"></i></a> 
                          <div> <br>
                             <div class="btn-group">
-              <a href="{{route('admin.show-invoice',['id'=>$customer->customer_id])}}" class="btn btn-success">View Invoice</a>
-              &nbsp;
-              <a href="{{route('admin.create-invoice',['id'=>$customer->customer_id])}}" class="btn btn-success">Generate Invoice</a>
-            
-            </div>  
-         
+                              <a href="{{route('admin.show-invoice',['id'=>$customer->customer_id])}}" class="btn btn-success">View Invoice</a>
+                              &nbsp;
+                              <a href="{{route('admin.create-invoice',['id'=>$customer->customer_id])}}" class="btn btn-success">Generate Invoice</a>
+                            
+                            </div>  
+                         
                          </div>   <br>
                            <?php $id=encrypt($customer->customer_id);?>
-                       <a href="{{route('admin.chat',['id'=>$id])}}" class="btn btn-primary btn-sm">Show Remarks</a>
+                      <!-- <a href="{{route('admin.chat',['id'=>$id])}}" class="btn btn-primary btn-sm ">Show Remarks</a>-->
+                        <button class="btn btn-primary btn-sm" id="remarks">Show Remarks</button>
 
-                            
-                                
                         <p class="text-secondary mb-1"><br>Type -<?php if($customer->type==1){echo 'Individual';}else if($customer->type==2){echo 'Business';}?></p>
                         <p class="text-muted font-size-sm">Service Name - {{$service_data->name}}</p>
                         <p class="text-secondary mb-1">Email -{{$customer->customer_email}}</p>
@@ -137,13 +136,127 @@
                      
                     </ul>
                   </div>
+                        <!--Show Remarks Start-->
+                        <div class="page-content" style="display: none;" id="show_remarks"> 
+                          <div class="chat-wrapper" style="overflow: scroll; height: 500px; padding: 0px 0px 38px 0px;">
+
+                           <div class="chat-header d-flex align-items-center" style="position: static;"> 
+                            <div class="chat-toggle-btn">
+                              <i class='bx bx-menu-alt-left'></i> 
+                            </div> 
+                            <div> 
+                              <h4 class="mb-1 font-weight-bold">{{$customer->customer_name}}</h4> 
+                              <div>
+                                 <a href="javascript:;" class="list-inline-item d-flex align-items-center text-secondary"><small class='bx bxs-circle me-1 chart-online'></small>Remarks</a>
+                              </div>
+                            
+                            </div> 
+                           
+                          </div>
+                          <div class="chat-content" style="margin-left: 0px; height: auto;"> 
+
+                           <?php 
+                           if(!empty($data)){
+                            foreach ($data as $key => $value) {
+                             ?>
+                             <div class="chat-content-leftside"> 
+                              <div class  ="d-flex">
+                               <img src="/assets/images/user.png" width="48"
+                               height="48" class="rounded-circle" alt /> 
+                               <div class="flex-grow-1 ms-2"> 
+                                {{-- <p class="mb-0 chat-time">{{$value->customer_name}}</p>  --}}  
+                                <p class="chat-left-msg">{{$value->first_name}} {{$value->last_name}}</p> 
+                                <p class="mb-0 chat-time">{{$value->user_type}}</p> 
+
+                              </div> 
+                            </div> 
+                          </div>
+
+                          <div class="chat-content-rightside">   
+                            <div class="d-flex ms-auto">
+                             <div class="flex-grow-1 me-2"> 
+                              <p class="mb-0 chat-time text-end" id="time-ago">
+
+                                <?php $dateTime = new DateTime($value->created_at);
+                                $now = new DateTime();
+
+                                $interval = $now->diff($dateTime);
+
+                                if ($interval->y > 0) {
+                                  $timeAgo = $interval->y . ' year' . ($interval->y > 1 ? 's' : '') . ' ago';
+                                } elseif ($interval->m > 0) {
+                                  $timeAgo = $interval->m . ' month' . ($interval->m > 1 ? 's' : '') . ' ago';
+                                } elseif ($interval->d > 0) {
+                                  $timeAgo = $interval->d . ' day' . ($interval->d > 1 ? 's' : '') . ' ago';
+                                } elseif ($interval->h > 0) {
+                                  $timeAgo = $interval->h . ' hour' . ($interval->h > 1 ? 's' : '') . ' ago';
+                                } elseif ($interval->i > 0) {
+                                  $timeAgo = $interval->i . ' minute' . ($interval->i > 1 ? 's' : '') . ' ago';
+                                } else {
+                                  $timeAgo = 'just now';
+                                }
+
+                                echo $timeAgo;
+                                ?> 
+
+                              </p>
+                              <p class="chat-right-msg">{{$value->remark}}</p> 
+                            </div>
+                            </div> 
+                          </div>  
+
+
+                          <?php } } ?> 
+
+                    </div> 
+                                     
+                                  <!--start chat overlay--> 
+                               <div class="overlay chat-toggle-btn-mobile"></div>
+                              <!--end chat overlay-->
+                          </div> 
+                      </div>
+                       <form id="remark_form">
+                                        <input type="hidden" name="team_member_id" value="{{$customer->team_member}}">
+                                        <input type="hidden" name="customer_id" value="{{$customer->customer_id}}">
+                                        <input type="hidden" name="role" value="{{$admin_data->user_type}}">
+                                        <input type="hidden" name="user_id" value="{{$admin_data->id}}">
+
+                                        {{@csrf_field()}}
+                                        <div class="chat-footer d-flex align-items-center" style="position:absolute; left:0;"> 
+                                         <div class="flex-grow-1 pe-2"> 
+                                          <div class="input-group"> 
+                                        <!--   <span class="input-group-text">
+                                              <i class='bx bx-smile'></i>
+                                            </span>  -->
+                                            <input type="text" class="form-control" name="remark" placeholder="Type Remark"> 
+                                          </div> 
+                                        </div> 
+                                        <div class="chat-footer-menu"> 
+                                     <!--  <a href="javascript:;">
+                                          <i class='bx bx-file'></i>
+                                        </a>  -->
+                                        <button type="submit"><i class='bx bxs-send'></i></button>
+                                        <!-- <a href="javascript:;"><i class='bx bx-microphone'></i></a> -->
+                                        <!-- <a href="javascript:;"><i class='bx bx-dots-horizontal-rounded'></i></a> -->
+                                      </div> 
+                                    </div> 
+                                  </form>
+                        <!--Show Remarks End-->    
+                      
                 </div>
+
               </div>
               <div class="col-lg-8">
+
              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+   <script type="text/javascript">
+     $('#remarks').click(function(){
+        $('#show_remarks').show();
+     });
+  </script>
 @include('admin.dashboard.footer')
--- resources/views/admin/dashboard/view_clients.blade.php
+++ resources/views/admin/dashboard/view_clients.blade.php
@@ -110,6 +110,7 @@
 								</tbody>
 							
 							</table>
+							<div>{{$data->links()}}</div>
 						</div>
 					</div>
 				</div>
