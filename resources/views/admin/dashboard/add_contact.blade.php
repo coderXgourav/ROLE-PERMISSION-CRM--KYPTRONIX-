6ba0c58e535c470fcabacb2776cc4fcdae3d239b
srimayeekyptronix1234 <srimayee.kyptronix@gmail.com>
2024-11-26 18:14:19 +0530

    commit

--- resources/views/admin/dashboard/add_contact.blade.php
+++ resources/views/admin/dashboard/add_contact.blade.php
@@ -268,7 +268,7 @@ width: 100% !important;
 												<div class="d-flex align-items-center gap-3">
 												<div class="form-check form-switch">
 													<input type="hidden" name="email_sms_manage" value="0">
-													<input class="form-check-input" name="email_sms_manage" type="checkbox" role="switch" id="flexSwitchCheckDefault7" >
+													<input class="form-check-input" value="1"  name="email_sms_manage" type="checkbox" role="switch" id="flexSwitchCheckDefault7" >
 													<label class="form-check-label" for="flexSwitchCheckDefault7">Email & SMS View </label>
 												</div>
 						                    </div>
